bfe2bc47db1b7f884232fbe2cdc6b78c75506661
Vert1cal-maker <128141917+Vert1cal-maker@users.noreply.github.com>
2023-04-20 20:21:30 +0300

    Update Product.js

--- 1-1/Product.js
+++ 1-1/Product.js
@@ -1,4 +1,4 @@
-function Product(name, description, price, brand, activeSize, quantity){
+function Product(name, description, price, brand, quantity){
     if(typeof name !== 'string'|| name === null){
         throw new Error("Name argument must be String")
     } else if(typeof description !== 'string'|| description === null){
@@ -17,7 +17,7 @@ function Product(name, description, price, brand, activeSize, quantity){
     this.brand = brand;
     this.quantity = quantity;
     this.sizes = ['XS', 'S', 'M', 'L', 'XL', 'XLL']
-    this.activeSize = this.searchSize(activeSize);
+    this.activeSize = this.sizes[0];
     this.date = require('./CurrentDate.js');
     this.reviews = [];
     this.images = ["short","boots","t-shirts"];
@@ -84,6 +84,91 @@ Product.prototype.getAverageRating = function(){
 Product.getNextId = function(){
     return ++ Product.counter;
 };
+
+Product.prototype.getName = function(){
+    return this.name;
+}
+Product.prototype.setName=function(arg){
+    this.name = arg;
+}
+Product.prototype.getDescription = function(){
+    return this.description;
+}
+Product.prototype.setDescription=function(arg){
+    this.name = arg;
+}
+Product.prototype.getPrice = function(){
+    return this.price;
+}
+Product.prototype.setPrice=function(arg){
+    this.price = arg;
+}
+Product.prototype.getBrand = function(){
+    return this.brand;
+}
+Product.prototype.setBrand=function(arg){
+    this.brand = arg;
+}
+Product.prototype.getQuantity = function(){
+    return this.quantity;
+}
+Product.prototype.setQuantity = function(arg){
+   this.quantity = arg;
+}
+
+Product.prototype.getSizes = function(){
+    return this.sizes;
+}
+Product.prototype.setSizes = function(arg){
+    if(typeof arg !== "object"){
+        throw new Error("must be Array");
+    }
+   this.sizes = arg;
+}
+
+Product.prototype.getActiveSize = function(){
+    return this.activeSize;
+}
+Product.prototype.setActiveSize = function(arg){
+   this.activeSize = this.searchSize(arg);
+}
+
+Product.prototype.getDate = function(){
+    return this.date;
+}
+Product.prototype.setActiveSize = function(arg){
+   this.date = arg;
+}
+
+Product.prototype.getReview = function(){
+    return this.reviews;
+}
+Product.prototype.setReview = function(arg){
+    if(typeof arg !== "object"){
+        throw new Error("must be Array");
+    }
+   this.reviews = arg;
+}
+
+Product.prototype.getReview = function(){
+    return this.reviews;
+}
+Product.prototype.setReview = function(arg){
+    if(typeof arg !== "object"){
+        throw new Error("must be Array");
+    }
+   this.reviews = arg;
+}
+
+Product.prototype.getImages= function(){
+    return this.images
+}
+Product.prototype.setImages = function(arg){
+    if(typeof arg !== "object"){
+        throw new Error("must be Array");
+    }
+   this.images = arg;
+}
 
 
 module.exports = Product;
